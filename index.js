d653b1c2803d3c9f50149eac3733973722b6af92
Simon Hanukaev <semenh@gmail.com>
2022-02-25 00:57:14 +0200

    added sourcebitNext plugin function

--- index.js
+++ index.js
@@ -1,26 +1,53 @@
+const path = require('path');
 require('dotenv').config();
 const Sourcebit = require('./lib/sourcebit');
 
 module.exports.fetch = (config, runtimeParameters, transformCallback) => {
-    if (!config) {
-        throw new Error('ERROR: Could not find a valid `sourcebit.js` configuration file.');
-    }
-
     if (typeof runtimeParameters === 'function') {
         transformCallback = runtimeParameters;
         runtimeParameters = {};
     }
 
+    const transformDataPromise = fetch(config, runtimeParameters, transformCallback)
+
+    if (typeof transformCallback !== 'function') {
+        return transformDataPromise;
+    }
+};
+
+module.exports.sourcebitNext = ({ config, runtimeParameters, transformCallback } = {}) => {
+    return function withSourcebit(nextConfig) {
+        return {
+            ...nextConfig,
+            // Since Next.js doesn't provide some kind of real "plugin system"
+            // we're (ab)using the `redirects` option here in order to hook into
+            // and block the `next build` and initial `next dev` run.
+            redirects: async () => {
+                await fetch(config, runtimeParameters, transformCallback);
+                return nextConfig.redirects ? nextConfig.redirects() : [];
+            }
+        }
+    }
+};
+
+async function fetch(config, runtimeParameters, transformCallback) {
+    if (!config) {
+        try {
+            const configPath = path.resolve(process.cwd(), 'sourcebit.js');
+            config = require(configPath);
+        } catch (error) {
+            if (error.code === 'MODULE_NOT_FOUND') {
+                throw new Error('Could not find sourcebit.js configuration file');
+            }
+            throw error;
+        }
+    }
     const instance = new Sourcebit({ runtimeParameters, transformCallback });
     const { plugins = [] } = config;
 
     instance.loadPlugins(plugins);
 
-    const transformData = instance.bootstrapAll().then(() => instance.transform());
-
-    if (typeof transformCallback !== 'function') {
-        return transformData;
-    }
-};
+    return instance.bootstrapAll().then(() => instance.transform());
+}
 
 module.exports.Sourcebit = Sourcebit;
